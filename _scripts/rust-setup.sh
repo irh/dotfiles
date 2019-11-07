917dba0dc2331f7bcf4e1dbcc3c5d8b7e785bb3e
irh <ian.r.hobson@gmail.com>
2019-11-07 13:14:47 +0100

    Update rust setup script

--- _scripts/rust-setup.sh
+++ _scripts/rust-setup.sh
@@ -1,3 +1,4 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 rustup install nightly
-cargo install cargo-watch sccache
+rustup component add rust-src rustfmt
+cargo install cargo-tree cargo-watch sccache

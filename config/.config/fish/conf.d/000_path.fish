115daac3bdfb58916552633f7e63aca506bb43b8
irh <ian.r.hobson@gmail.com>
2021-06-17 22:35:48 +0200

    Add ruby setup

--- config/.config/fish/conf.d/000_path.fish
+++ config/.config/fish/conf.d/000_path.fish
@@ -1,10 +1,18 @@
 set -x PATH \
   /opt/homebrew/bin \
   /usr/local/bin \
-  $HOME/.cargo/bin \
-  $HOME/dev/flutter/bin \
   $PATH
 
 set -x BREW_DIR (brew --prefix)
 
-set -g fish_user_paths $BREW_DIR/opt/llvm/bin $fish_user_paths
+set -x PATH \
+  $BREW_DIR/opt/make/libexec/gnubin \
+  $BREW_DIR/opt/ruby/bin \
+  $BREW_DIR/lib/ruby/gems/3.0.0/bin \
+  $HOME/.cargo/bin \
+  $HOME/dev/flutter/bin \
+  $PATH
+
+set -g fish_user_paths \
+  $BREW_DIR/opt/llvm/bin \
+  $fish_user_paths

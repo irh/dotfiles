15e1021c28af3fb7f8d047f43459c1253c6198b8
irh <ian.r.hobson@gmail.com>
2022-04-22 12:43:45 +0200

    julia: Update default packages

--- _julia/packages.jl
+++ _julia/packages.jl
@@ -1,7 +1,11 @@
 println("Installing Julia packages...")
 
-Pkg.add("IJulia")
 Pkg.add("DSP")
+Pkg.add("FFTW")
+Pkg.add("Interact")
+Pkg.add("IJulia")
+Pkg.add("LanguageServer")
+Pkg.add("WAV")
 
 println("Building Julia packages")
 

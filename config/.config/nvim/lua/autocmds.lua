6e08a5689702de49cc3b05f097e7368d7fbf3c4f
irh <ian.r.hobson@gmail.com>
2022-04-24 09:07:08 +0200

    neovim: Improve the statusline

--- config/.config/nvim/lua/autocmds.lua
+++ config/.config/nvim/lua/autocmds.lua
@@ -1,11 +1,3 @@
--- Hide the statusline in the file tree
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "NvimTree*",
-  callback = function(args)
-    vim.opt_local.statusline = ' '
-  end,
-})
-
 -- Git commit messages and text files
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "gitcommit,markdown",
@@ -17,3 +9,4 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "rust",
   command = "setlocal cc=101 | compiler cargo",
 })
+
--- config/.config/nvim/lua/statusline.lua
+++ config/.config/nvim/lua/statusline.lua
@@ -1,6 +1,51 @@
-vim.opt.statusline = '%#CursorColumn#'
-    .. ' %f ' -- file
-    .. ' %m ' -- modified
-    .. '%=' -- separator
-    .. '%l:%c' -- line/column
-    .. ' %y' -- file type
+local set_active = function()
+  return '%#StatusLine#'
+      -- file path
+      .. ' %f '
+      -- separator
+      .. '%='
+      -- line:column
+      .. ' %l:%c '
+      -- file type
+      .. ' %y '
+end
+
+local set_inactive = function()
+  return '%#StatusLineNC#'
+      -- file path
+      .. ' %f'
+end
+
+local set_inactive = function()
+  return '%#StatusLineNC#'
+      -- file path
+      .. ' %f'
+end
+
+vim.api.nvim_create_autocmd({"WinEnter", "BufEnter"}, {
+  pattern = "*",
+  callback = function(args)
+    vim.opt_local.statusline = set_active()
+  end,
+})
+
+vim.api.nvim_create_autocmd({"WinLeave", "BufLeave"}, {
+  pattern = "*",
+  callback = function(args)
+    vim.opt_local.statusline = set_inactive()
+  end,
+})
+
+vim.api.nvim_create_autocmd({"WinEnter", "BufEnter"}, {
+  pattern = "NvimTree*",
+  callback = function(args)
+    vim.opt_local.statusline = " "
+  end,
+})
+
+vim.api.nvim_create_autocmd({"WinLeave", "BufLeave"}, {
+  pattern = "NvimTree*",
+  callback = function(args)
+    vim.opt_local.statusline = " "
+  end,
+})

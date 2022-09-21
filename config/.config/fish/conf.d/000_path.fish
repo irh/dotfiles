4ec308ac03fa145209fbd510e014972e8aea239e
irh <ian.r.hobson@gmail.com>
2022-09-21 17:23:46 +0200

    fish: Path setup cleanup

--- config/.config/fish/conf.d/000_path.fish
+++ config/.config/fish/conf.d/000_path.fish
@@ -1,28 +1,18 @@
-set -x PATH \
-  /usr/local/bin \
-  $HOME/.cargo/bin \
-  $PATH
+fish_add_path -g /usr/local/bin
+fish_add_path -g $HOME/.cargo/bin
 
 switch (uname)
 case Darwin
   set -x PNPM_HOME "/Users/ian/Library/pnpm"
-
-  set -x PATH \
-    /opt/homebrew/bin \
-    $HOME/.homebrew/bin \
-    "$PNPM_HOME" \
-    $PATH
+  set -x BREW_DIR "/opt/homebrew"
+  fish_add_path -g $PNPM_HOME
 end
 
-if type -q brew
-  set -x BREW_DIR (brew --prefix)
-  set -x PATH \
-    $BREW_DIR/opt/make/libexec/gnubin \
-    $BREW_DIR/opt/ruby/bin \
-    $BREW_DIR/lib/ruby/gems/3.0.0/bin \
-    $PATH
-  set -g fish_user_paths \
-    $BREW_DIR/opt/llvm/bin \
-    $fish_user_paths
+if type -q $BREW_DIR/bin/brew
+  fish_add_path -g $BREW_DIR/bin
+  fish_add_path -g $BREW_DIR/opt/make/libexec/gnubin
+  fish_add_path -g $BREW_DIR/opt/ruby/bin
+  fish_add_path -g $BREW_DIR/lib/ruby/gems/3.0.0/bin
+  fish_add_path -g $BREW_DIR/opt/llvm/bin
 end
 

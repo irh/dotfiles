1a022f3cfad989770b7c36ad209a1002b60c54e2
Ian Hobson <ian.r.hobson@gmail.com>
2023-09-20 10:53:49 +0200

    rust: Add rust-analyzer as a rustup component

--- _scripts/rust-setup.sh
+++ _scripts/rust-setup.sh
@@ -1,6 +1,9 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
-rustup component add rust-src rustfmt
+rustup component add \
+  rust-analyzer \
+  rust-src \
+  rustfmt
 
 cargo install \
   cargo-bloat \

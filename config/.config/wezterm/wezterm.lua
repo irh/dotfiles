ff7a6aa8075846d6d4ad09442a511025959f40e7
Ian Hobson <ian.r.hobson@gmail.com>
2023-10-19 22:52:11 +0200

    wezterm: bump default font size

--- config/.config/wezterm/wezterm.lua
+++ config/.config/wezterm/wezterm.lua
@@ -18,7 +18,7 @@ config.native_macos_fullscreen_mode = true
 
 -- Font
 config.font = wezterm.font_with_fallback { 'Input', 'Apple Color Emoji' }
-config.font_size = 11
+config.font_size = 12
 
 -- Color scheme
 local colors = wezterm.color.get_builtin_schemes()['Solarized Dark (Gogh)']

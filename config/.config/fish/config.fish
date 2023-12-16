6d15c6cbdd8f5550707734ec4868360133908667
Ian Hobson <ian.r.hobson@gmail.com>
2023-12-16 13:59:32 +0100

    helix: setup for tree-sitter-koto testing

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -5,6 +5,7 @@ set -x LC_ALL en_US.UTF-8
 set -x ANDROID_NDK_HOME ~/Library/android/sdk/ndk/current
 set -x EDITOR nvim
 set -x RUSTC_WRAPPER sccache
+set -x HELIX_RUNTIME ~/.config/helix/runtime
 
 if type -q mcfly
   mcfly init fish | source
--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -36,5 +36,13 @@ return {
         use_languagetree = true,
       },
     }
+
+    local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
+    parser_config.koto = {
+      install_info = {
+        url = "~/dev/koto/tree-sitter-koto",
+        files = { 'src/parser.c', 'src/scanner.c' },
+      },
+    }
   end
 }

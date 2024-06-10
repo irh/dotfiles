c1358cf2300606a33faf43e39118d0ecbad69897
Ian Hobson <ian.r.hobson@gmail.com>
2024-06-10 15:07:40 +0200

    neovim: Add flash.nvim

--- config/.config/nvim/lua/plugins/init.lua
+++ config/.config/nvim/lua/plugins/init.lua
@@ -222,6 +222,19 @@ return {
     event = 'VeryLazy',
   },
 
+  -- flash.nvim
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
+
   -- Redirect output of commands into a buffer
   {
     'AndrewRadev/bufferize.vim',

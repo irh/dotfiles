4404c97bbc1ec2fe261f6c14891574f31f665034
Ian Hobson <ian.r.hobson@gmail.com>
2024-08-13 09:32:19 +0200

    Update the new machine install script

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,9 +1,16 @@
+#!/bin/zsh
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  cd _homebrew
-  brew bundle --verbose
+  if ! command -v brew &> /dev/null
+  then
+    echo "homebrew isn't available, exiting"
+    exit 1
+  fi
+
+  brew bundle --verbose --file _homebrew/Brewfile
+
   # Link lldb-vscode, don't link the rest of LLVM
   ln -s $(brew --prefix)/opt/llvm/bin/lldb-vscode $(brew --prefix)/bin/
-  cd ..
 fi
 
 source _scripts/stow.sh
@@ -11,13 +18,15 @@ source _scripts/stow.sh
 source _scripts/rust-setup.sh
 source _scripts/node-setup.sh
 
-pip3 install -r _pip/packages3.txt
-julia _julia/packages.jl
+# pip3 install -r _pip/packages3.txt
+# julia _julia/packages.jl
 
 source _scripts/neovim-setup.sh
 source _scripts/tmux-setup.sh
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   stow Library
-  source _scripts/setup-mac-preferences.sh
+  sudo source _scripts/setup-mac-preferences.sh
+
+  brew bundle --verbose --file _homebrew/Brewfile-casks
 fi

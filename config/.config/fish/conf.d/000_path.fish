330a88b4c4cabbe3cd50011d922124308535a9ba
Ian Hobson <ian.r.hobson@gmail.com>
2024-09-27 18:01:35 +0200

    helix: Add a mapping to open the current word in Dash

--- config/.config/fish/conf.d/000_path.fish
+++ config/.config/fish/conf.d/000_path.fish
@@ -1,19 +1,19 @@
 fish_add_path -g /usr/local/bin
 fish_add_path -g $HOME/.local/bin
 fish_add_path -g $HOME/.cargo/bin
+fish_add_path -g $HOME/Sync/scripts
 
 switch (uname)
-case Darwin
-  set -x PNPM_HOME "/Users/ian/Library/pnpm"
-  set -x BREW_DIR "/opt/homebrew"
-  fish_add_path -g $PNPM_HOME
+    case Darwin
+        set -x PNPM_HOME /Users/ian/Library/pnpm
+        set -x BREW_DIR /opt/homebrew
+        fish_add_path -g $PNPM_HOME
 end
 
 if type -q $BREW_DIR/bin/brew
-  fish_add_path -g $BREW_DIR/bin
-  fish_add_path -g $BREW_DIR/opt/make/libexec/gnubin
-  fish_add_path -g $BREW_DIR/opt/ruby/bin
-  fish_add_path -g $BREW_DIR/lib/ruby/gems/3.0.0/bin
-  # fish_add_path -g $BREW_DIR/opt/llvm/bin
+    fish_add_path -g $BREW_DIR/bin
+    fish_add_path -g $BREW_DIR/opt/make/libexec/gnubin
+    fish_add_path -g $BREW_DIR/opt/ruby/bin
+    fish_add_path -g $BREW_DIR/lib/ruby/gems/3.0.0/bin
+    # fish_add_path -g $BREW_DIR/opt/llvm/bin
 end
-

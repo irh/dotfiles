6b8231ac0af8854a57fa348305711cd99c6ed509
Ian Hobson <ian.r.hobson@gmail.com>
2024-09-27 16:21:26 +0200

    Use helix as the default editor

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -3,10 +3,10 @@ set -x LC_ALL en_US.UTF-8
 
 # env vars
 set -x ANDROID_NDK_HOME ~/Library/android/sdk/ndk/current
-set -x EDITOR nvim
+set -x EDITOR hx
 set -x RUSTC_WRAPPER sccache
 set -x HELIX_RUNTIME ~/.config/helix/runtime
 
 if type -q mcfly
-  mcfly init fish | source
+    mcfly init fish | source
 end

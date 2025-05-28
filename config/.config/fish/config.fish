0c1b45d51bbca13424e8ed902f070a2f684e9f7a
Ian Hobson <ian.r.hobson@gmail.com>
2025-05-28 09:15:58 +0200

    Add ollama env var

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -4,8 +4,9 @@ set -x LC_ALL en_US.UTF-8
 # env vars
 set -x ANDROID_NDK_HOME ~/Library/android/sdk/ndk/current
 set -x EDITOR hx
-set -x RUSTC_WRAPPER sccache
+set -x OLLAMA_API_BASE http://studio:11434
 set -x HELIX_RUNTIME ~/.config/helix/runtime
+set -x RUSTC_WRAPPER sccache
 
 set -x PYENV_ROOT $HOME/.pyenv
 

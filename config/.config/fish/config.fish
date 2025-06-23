20273971a667260dcb2f79a4a6ac406c6545433e
Ian Hobson <ian.r.hobson@gmail.com>
2025-06-23 20:25:43 +0200

    bacon: Add prefs.toml

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -3,12 +3,12 @@ set -x LC_ALL en_US.UTF-8
 
 # env vars
 set -x ANDROID_NDK_HOME ~/Library/android/sdk/ndk/current
+set -x BACON_PREFS ~/.config/bacon/prefs.toml
 set -x EDITOR hx
-set -x OLLAMA_API_BASE http://studio:11434
 set -x HELIX_RUNTIME ~/.config/helix/runtime
-set -x RUSTC_WRAPPER sccache
-
+set -x OLLAMA_API_BASE http://studio:11434
 set -x PYENV_ROOT $HOME/.pyenv
+set -x RUSTC_WRAPPER sccache
 
 if type -q mcfly
     mcfly init fish | source
